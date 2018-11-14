a2cacbb01b49c8f3e4ee3c3789c8852c92d9b52b
JinkeCao <jcao2014@outlook.com>
2018-11-14 18:50:29 +0800

    Update service_hist.sql

--- service_hist.sql
+++ service_hist.sql
@@ -2,3 +2,15 @@ SELECT s, count(1)
 FROM etl.idata_log
 LATERAL VIEW explode(split(running_services,"\;")) a AS s
 GROUP BY s;
+
+select c.ser, c.con,count(distinct c.dvc) 
+from (
+select b.dvc as dvc, b.s as ser, count(distinct b.day_time) as con 
+from (
+select dvc, s, day_time
+from etl.idata_log
+lateral view explode(split(running_services,"\;")) a as s
+) b
+group by b.dvc, b.s
+) c
+group by c.ser, c.con
