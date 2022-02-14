9df6d5bb49ab68168538f5b0bb0f3ba992ca4ce1
JinkeCao <jcao2014@outlook.com>
2022-02-14 20:01:32 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -5,7 +5,7 @@ Spyder Editor
 This is a temporary script file.
 """
 
-from os import system
+from os import system, listdir
 # from __future__ import unicode_literals
 # import youtube_dl
 from func_timeout import func_set_timeout, FunctionTimedOut
@@ -14,7 +14,10 @@ from time import sleep
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.keys import Keys
 from urllib.parse import urlencode
-
+from paramiko import SSHClient, AutoAddPolicy
+from scp import SCPClient
+# import bilili
+# b = bilili()
 
 @func_set_timeout(99)
 def link2bso(link):
@@ -48,8 +51,48 @@ def timered_link2bso(link):
         bso = BeautifulSoup(str(e), 'html.parser')
     return bso
 
+@func_set_timeout(999)
+def gbili(url):
+    # system(
+    #     r'bilili --danmaku no -d C:\Users\jkcao\Videos\tmp' 
+    #     + r' --playlist-type no -y ' 
+    #     + r'https:'
+    #     + url
+    #     )
+    system(
+        r'you-get --no-caption -o C:\Users\jkcao\Videos\tmp\ '
+        + url
+        )
+
+def timered_gbili(url):
+    try: gbili(url)
+    except FunctionTimedOut: print('gibili timeout')
+    except Exception as e: print(str(e))
+
+@func_set_timeout(999)    
+def ubili(tmp, rp):
+    with SSHClient() as ssh:
+        ssh.set_missing_host_key_policy(AutoAddPolicy())
+        ssh.connect(
+            hostname=''
+            , port=0
+            , username=''
+            , password=''
+            )
+        with SCPClient(ssh.get_transport()) as scp:
+            scp.put(
+                tmp
+                ,recursive=True
+                ,remote_path=rp
+                )
+def timered_ubili(tmp, rp):
+    try: ubili(tmp, rp)
+    except FunctionTimedOut: print('ubili timeout')
+    except Exception as e: print(str(e))
+
 # system(r"bilili --danmaku 'no' https://www.bilibili.com/video/BV1444y1H7hy")
 # bilili --danmaku no -d D:\20220208_dy\ --playlist-type no -y https://www.bilibili.com/video/BV1bE411T7yk
+# 【教程】平面设计零基础入门到品牌进阶全套系统课程 软件基础篇（PS+AI+精修+合成） - bilibili
 
 for k in (
         'AI合成'
@@ -58,7 +101,7 @@ for k in (
         ,'合成音频'
         ,'伪造音频'
         ,'deepfake'
-        ,'deepfake voice'
+        ,'deepfake-voice'
         ,'假音'
         ,'仿声'
         ,'声音模仿'
@@ -74,35 +117,39 @@ for k in (
             if len(bso.find_all(class_='video-item matrix'))>0:
                 for li in bso.find_all(class_='video-item matrix'):
                     try:
-                        system(r'bilili --danmaku no -d C:\Users\jkcao\Videos' + r'/' + k + r'/' + r' --playlist-type no -y ' + r'https:'+li.a['href'].split('?')[0])
-                        sleep(9)
+                        if r'http:' not in li.a['href'].split('?')[0]:
+                            url = r'http:' + li.a['href'].split('?')[0]
+                        else:
+                            url = li.a['href'].split('?')[0]
+                        try:
+                            timered_gbili(url)
+                            sleep(9)
+                        except: pass
+                        s = 0
+                        while r'.download' in ' '.join(listdir(r'C:\Users\jkcao\Videos\tmp')) or len(listdir(r'C:\Users\jkcao\Videos\tmp')) != 1:
+                            sleep(9)
+                            s += 1
+                            if s > 111: break
+                        
+                        ld = listdir(r'C:\Users\jkcao\Videos\tmp')
+                        try:
+                            timered_ubili(
+                            tmp='C:\\Users\\jkcao\\Videos\\tmp\\' + ld[0]
+                            , rp = '/iflytek/jkcao/bili/' + k
+                            )
+                            sleep(9)
+                        except: pass
+                        while len(listdir(r'C:\Users\jkcao\Videos\tmp')) > 0:
+                            for d in ld:
+                                system(
+                                    r'del /f /s /q "'
+                                    + 'C:\\Users\\jkcao\\Videos\\tmp\\'
+                                    + d
+                                    + '"'
+                                    )
+                                sleep(1)
                     except: continue
             else: break
-        except: continue
-
-
-
-
-# # youtube-dl -f 2 https://www.bilibili.com/video/BV1mZ4y1c73Q
-# x = {
-#       'h': [r'https://www.bilibili.com/video/BV1TR4y1n7YU']
-#         }
-# for i in x:
-#     try:
-#         ydl_opts = {#'sleep_interval': 9
-#                     # ,'socket_timeout':99
-#                     'format':r'2'
-#                     ,'outtmpl':r'd:/20220208_dy/' + r'%(title)s.%(ext)s'
-#                     # ,'ignoreerrors': True
-#                     # ,'continuedl':True
-#                     # ,'listformats':True
-#                     # ,'forceurl':True
-#                     # ,'retries': 5
-#                     }
-#         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#             ydl.download(x[i])
-
-            
-#     except Exception as e:
-#         print(str(e))
-
+        except Exception as e:
+            print(str(e))
+            continue
